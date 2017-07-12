46550edee77e75b4c7645dababc207ed21f14471
PeterLock <pclo2333@gmail.com>
2017-07-12 10:35:36 +0900

    Add files via upload

--- Events/src/sample/Controller.java
+++ Events/src/sample/Controller.java
@@ -1,17 +1,50 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
 import javafx.scene.control.TextField;
 import javafx.event.ActionEvent;
 
 public class Controller {
     @FXML
     private TextField nameField;
+    @FXML
+    private Button helloButton;
+    @FXML
+    private Button byeButton;
+    @FXML
+    private CheckBox ourCheckBox;
+
+    public void initialize(){
+        helloButton.setDisable(true);
+        byeButton.setDisable(true);
+    }
 
     @FXML
     public void onButtonClicked(ActionEvent e) {
+        if(e.getSource().equals(helloButton)){
+            System.out.println("Hello " + nameField.getText());
+        } else if(e.getSource().equals(byeButton)){
+            System.out.println("Bye " + nameField.getText());
+        }
+        if(ourCheckBox.isSelected()){
+            nameField.clear();
+            helloButton.setDisable(true);
+            byeButton.setDisable(true);
+        }
+    }
 
-        System.out.println("Hello, " + nameField.getText());
-        System.out.println("The following button was clicked " + e.getSource());
+    @FXML
+    public void handleKeyRelease(){
+        String text = nameField.getText();
+        boolean disableButton = text.isEmpty() || text.trim().isEmpty();
+        helloButton.setDisable(disableButton);
+        byeButton.setDisable(disableButton);
+    }
+
+    @FXML
+    public void handleChange(){
+        System.out.println("The checkbox is " + (ourCheckBox.isSelected() ? " checkbox" : " not checked"));
     }
 }
